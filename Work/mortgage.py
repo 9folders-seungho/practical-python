8eca855fea57ef63053337f19c675a15b341c3e3
9folders-seungho <seungho.jung@9folders.com>
2022-01-12 09:59:13 +0900

    1.3 ~ 1.7

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,30 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+mortgage = float(500000)
+total_paid = 0.0
+months = 0
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000.0
+
+while mortgage > 0:
+    paid = 2684.11
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        paid += extra_payment
+
+    if mortgage < paid:
+        total_paid += mortgage
+        mortgage = 0
+    else:
+        mortgage = mortgage * (1.0 + (0.05 / 12)) - paid
+        total_paid += paid
+
+    months += 1
+    print("{} {} {}".format(months, total_paid, mortgage))
+
+print("Total paid {}".format(total_paid))
+print("Months {}".format(months))
+
--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,27 @@
 # pcost.py
 #
 # Exercise 1.27
+import sys
+
+def portfolio_cost(filename):
+    total_cost = 0
+
+    try:
+        with open(filename, 'rt') as f:
+            next(f).split(',')
+            for line in f:
+                row = line.split(',')
+                total_cost += float(row[1]) * float(row[2])
+    except Exception as e:
+        print(f"ERROR {e}")
+
+    return total_cost        
+
+
+if len(sys.argv) == 2:
+    filename = sys.argv[1]
+else:
+    filename = 'Data/portfolio.csv'
+
+cost = portfolio_cost(filename)
+print('Total cost:', cost)
